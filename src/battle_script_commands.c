b36e795fbbf42bfb5b8cdcf54a70a64f4259bf85
kaistella <59009017+kaistella@users.noreply.github.com>
2022-05-10 13:58:00 +0800

    gen 6 exp share build

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3853,7 +3853,7 @@ static void Cmd_getexp(void)
     s32 i; // also used as stringId
     u8 holdEffect;
     s32 sentIn;
-    u16 *exp = &gBattleStruct->expValue;
+    u32 *exp = &gBattleStruct->expValue;
 
     gBattlerFainted = GetBattlerForBattleScript(gBattlescriptCurrInstr[1]);
     sentIn = gSentPokesToOpponent[(gBattlerFainted & 2) >> 1];
